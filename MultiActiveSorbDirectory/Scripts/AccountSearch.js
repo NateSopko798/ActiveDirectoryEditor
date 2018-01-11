88c5d7588260b672606a3c8703a003560cf0a3a1
nksopko@buffalo.edu <Motox#798>
2018-01-11 16:14:30 -0500

    Bootbox testing with form

--- MultiActiveSorbDirectory/Scripts/AccountSearch.js
+++ MultiActiveSorbDirectory/Scripts/AccountSearch.js
@@ -1,5 +1,3 @@
-
-
 var $rows = $('#userTable tr').not('thead tr');
 $('#search').keyup(function () {
     var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
@@ -12,16 +10,36 @@ $('#search').keyup(function () {
 
 $('#userTable tbody').on('click', 'tr', function () {
     var table = $(this).closest("table");
-    $("tr", table).removeClass("highlight");
     $(this).addClass("highlight");
-    //window.location.href = $(this).data('');
-    alert($(this).find("td:first").text());
-    //setTableValues();
-});
+    //bootbox.alert("This is the default alert!\nAlso: " + $(this).find("td:first").text());
+    var modal = bootbox.dialog({
+        message: $(".form-content").html(),
+        title: "Your awesome modal",
+        buttons: [
+          {
+              label: "Save",
+              className: "btn btn-primary pull-left",
+              callback: function () {
 
-//$(function () {
-//    var userTable = $('#userTable').DataTable({
-//        "dom": '<"top"i>rt<"bottom"flp><"clear">',
-//        "bSort": true
-//    });
-//});
+                  alert($('form #email').val());
+
+                  return false;
+              }
+          },
+          {
+              label: "Close",
+              className: "btn btn-default pull-left",
+              callback: function () {
+                  console.log("just do something on close");
+              }
+          }
+        ],
+        show: false,
+        onEscape: function () {
+            modal.modal("hide");
+        }
+    });
+
+    modal.modal("show");
+    $("tr", table).removeClass("highlight");
+});
