3bc215669c432d0b645845119441f73418e59e86
Nate Sopko <nksopko@buffalo.edu>
2018-02-12 10:50:31 -0500

    Update EditUserController.cs

--- MultiActiveSorbDirectory/Controllers/EditUserController.cs
+++ MultiActiveSorbDirectory/Controllers/EditUserController.cs
@@ -13,7 +13,7 @@ namespace MultiActiveSorbDirectory.Controllers
     {
         static DirectoryEntry createDirectoryEntry()
         {
-            DirectoryEntry directoryEntry = new DirectoryEntry("LDAP://OU=Users,OU=Harlem Road,DC=multisorb,DC=com", "Administrator", "325H@l3m!");
+            DirectoryEntry directoryEntry = new DirectoryEntry("LDAP://OU=Users,OU=subgroupname,DC=contoso,DC=com", "username", "password");
             return directoryEntry;
         }
 
